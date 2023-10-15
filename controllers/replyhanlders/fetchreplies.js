1c21316f7b6a3d3fb39239a7f9a9559b10aa1170
Debarun <paldebarun27@gmail.com>
2023-10-16 02:33:16 +0530

    the reply section is on built

---
+++ controllers/replyhanlders/fetchreplies.js
@@ -0,0 +1,40 @@
+const replySchema=require('../../models/reply');
+const commentSchema = require('../../models/comments');
+const mongoose=require('mongoose');
+
+exports.fetchblogcomments = async (req, res) => {
+    try {
+        const {comment_id} = req.body;
+        console.log("blog id ", comment_id);
+        const commentId = new mongoose.Types.ObjectId(comment_id);
+        const comment = await commentSchema.findById(commentId);
+
+        if (!comment) {
+            return res.status(400).json({
+                success: false,
+                message: "The comment is not found"
+            });
+        }
+
+        
+       
+
+        const commentreplyids = comment.replies;
+
+        const replies = await replySchema.find({ _id: { $in: commentreplyids } });
+
+
+
+        return res.status(200).json({
+            success: true,
+            replies: replies
+        });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+}
