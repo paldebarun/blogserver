ccab8744856d15d041568ebd78ddf57af6bec516
Debarun <paldebarun27@gmail.com>
2023-10-17 16:05:18 +0530

    the reply is under modification

--- controllers/replyhanlders/createreply.js
+++ controllers/replyhanlders/createreply.js
@@ -1,50 +1,51 @@
-const replySchema=require('../../models/reply');
-const commentschema=require('../../models/comments');
-const mongoose=require('mongoose');
-const UserSchema=require('../../models/user');
+const replySchema = require('../../models/reply');
+const commentSchema = require('../../models/comments');
+const UserSchema = require('../../models/user'); // Import UserSchema
+const mongoose = require('mongoose');
 
-exports.createreply=async (req,res)=>{
+exports.createreply = async (req, res) => {
+    try {
+        const { user_id, body, comment_id } = req.body;
 
-try{
-  
-    const {user_id,body,comment_id}=req.body;
+        const userid = new mongoose.Types.ObjectId(user_id);
+        const user = await UserSchema.findById(userid); // Corrected the findById call
 
-    const userid=new mongoose.Types.ObjectId(user_id);
-    const user=await UserSchema.findById({userid});
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
 
-   
-    const replyobj=new replySchema({
-        user_id,body,comment_id,autherName:user.autherName
-    });
-
-    const savedreply=await replyobj.save();
-
-    if(!savedreply){
-        return res.status(404).json({
-            success:false,
-            message:"the reply couldn't be saved"
+        const replyObj = new replySchema({
+            user_id: userid, // Store the user_id as a mongoose ObjectId
+            body,
+            comment_id,
+            authorName: user.authorName, // Fixed the typo here
         });
-    }
-    const comment = await commentschema.findById(comment_id);
-        comment.replies.push(savedreply._id);
+
+        const savedReply = await replyObj.save();
+
+        if (!savedReply) {
+            return res.status(404).json({
+                success: false,
+                message: "The reply couldn't be saved",
+            });
+        }
+
+        const comment = await commentSchema.findById(comment_id);
+        comment.replies.push(savedReply._id);
         await comment.save();
 
-
-    return res.status(200).json({
-        success:true,
-        message:"the reply is successfully posted"
-    })
-
-
-
-}
-catch(error){
-
-    return res.status(400).json({
-        success:false,
-        message:"there was an error while creating the reply"
-    })
-}
-
-
+        return res.status(200).json({
+            success: true,
+            message: "The reply is successfully posted",
+        });
+    } catch (error) {
+        console.error(error); // Log the error for debugging
+        return res.status(500).json({
+            success: false,
+            message: "There was an error while creating the reply",
+        });
+    }
 }
