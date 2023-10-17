a58cfc1c84a8f640e72e3aa5c84dd62fecff08ec
Debarun <paldebarun27@gmail.com>
2023-10-17 18:55:43 +0530

    the process is still on

--- controllers/replyhanlders/createreply.js
+++ controllers/replyhanlders/createreply.js
@@ -40,6 +40,7 @@ exports.createreply = async (req, res) => {
         return res.status(200).json({
             success: true,
             message: "The reply is successfully posted",
+            savedReply:savedReply
         });
     } catch (error) {
         console.error(error); // Log the error for debugging
